0957f39cb8ec03c7dc2e8e7965ac9da5dd8fad98
qiping <pksanwei@163.com>
2017-06-29 14:17:42 +0800

    周围学校 最近学校

--- project/app/v1/Application/Home/Controller/IndexController.class.php
+++ project/app/v1/Application/Home/Controller/IndexController.class.php
@@ -1,12 +1,41 @@
 <?php
 namespace Home\Controller;
+use Home\Model\Map;
 use Think\Controller;
 use Think\Model;
 
 class IndexController extends Controller {
-    public function index(){
-        $this->display();
+
+    //获取客户信息
+    protected function customerInfo()
+    {
+        $customer = M('customer');
+        $map['longitude_y'] = array('GT',0);
+        $map['latitude_x'] = array('GT',0);
+        $res = $customer->where($map)->field('customer_id,name,longitude_y,latitude_x')->select();
+        return $res;
     }
+
+    //所有客户的地址信息
+    public function location()
+    {
+        $res = $this->customerInfo();
+        sendSuccess($res);
+    }
+
+    //最近的一个客户
+    public function nearby()
+    {
+        $latitude_x = trim($_GET['latitude_x']);
+        $latitude_y = trim($_GET['longitude_y']);
+        $res = $this->customerInfo();
+
+        $map = new Map();
+        $data = $map->getCustomerInfo($latitude_x,$latitude_y,$res);
+
+        sendSuccess($data);
+    }
+
     public function test()
     {
         $data = array(
---
+++ project/app/v1/Application/Home/Model/Map.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/29
+ * Time: 10:32
+ */
+
+namespace Home\Model;
+
+
+class Map
+{
+    private static $EARTH_RADIUS = 6378.137;//地球半径,单位千米
+
+    protected $customer = array();//客户信息
+    protected $length = 0;//最短距离
+
+    private static function rad($d)
+    {
+        return $d * pi() / 180.0;
+    }
+
+    /**
+     *
+     * @param lat1 第一个纬度
+     * @param lng1第一个经度
+     * @param lat2第二个纬度
+     * @param lng2第二个经度
+     * @return 两个经纬度的距离
+     */
+    public function  getCustomerInfo( $lat1, $lng1,$data)
+    {
+        for($i=0,$len=count($data);$i<$len;$i++)
+        {
+            $lat2 = $data['latitude_x'];
+            $lng2 = $data['longitude_y'];
+
+            $radLat1 = self::rad($lat1);
+            $radLat2 = self::rad($lat2);
+            $a = $radLat1 - $radLat2;
+            $b = self::rad($lng1) - self::rad($lng2);
+
+            $s = 2 * asin(sqrt(pow(sin($a/2),2) +
+                    cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)));
+            $s = $s * self::$EARTH_RADIUS;
+            $s = round($s * 10000) / 10000;
+
+            if($this->length == 0 || $this->length > $s){
+                $this->customer = $data[$i];
+                $this->length = $s;
+            }
+        }
+        return $this->customer;
+    }
+}

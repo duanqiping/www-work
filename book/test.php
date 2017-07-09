f9ae5dfa784eac00d33dd110a48aef1181db60f6
qiping <pksanwei@163.com>
2017-07-10 00:34:39 +0800

    时间函数 处理

--- book/test.php
+++ book/test.php
@@ -1,21 +1,34 @@
 <?php  
+	
+	date_default_timezone_set("Asia/Shanghai");
     
 	$time = time();
-	var_dump($time);
 	
-	echo (date('Y-W',$time))	;
+	echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1-7,date("Y"))),"\n"; //本周起始时间 周一为一周的开始
 	
-	//var_dump($db);
+	echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),1,date("Y"))),"\n"; //本月起始时间  1498838399 1498838400
 	
+	
+	echo date("Y-m-d H:i:s",mktime(0,0,0,1,1,date("Y", time())))."<br>"; //本年起始时间
+	
+	function rankChoiceRule($flag)
 	{
-    "_id" : 1,
-    "user_id" : "15",
-    "customer_id" : "31",
-    "score_id" : 30,
-    "cycles" : 26,
-    "time" : 550,
-    "add_time" : 1499394368,
-    "length" : 10400
-}
+		if($flag == 'year'){
+			return  date("Y-m-d H:i:s",mktime(0,0,0,1,1,date("Y", time()))) ;//本年起始时间
+		}else if($flag == 'month'){
+			return  date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),1,date("Y"))) ;//本月起始时间
+		}else{
+			return  date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1-7,date("Y"))) ;//本周起始时间
+		}
+	}
+	
+	echo $time=strtotime(rankChoiceRule('year')),"\n";
+	echo $time=strtotime(rankChoiceRule('month')),"\n";
+	echo $time=strtotime(rankChoiceRule('week')),"\n";
+	
+	
+		
+	
+	
+	
 
-score.z_rank_m_34695.find()

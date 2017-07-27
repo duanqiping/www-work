91a91978d688d67b89c08e27af02e9e20618d0de
qiping <pksanwei@163.com>
2017-07-27 16:57:01 +0800

    工单系统 页面初步整合完成

--- project/back/Application/Admin/Controller/WoController.class.php
+++ project/back/Application/Admin/Controller/WoController.class.php
@@ -11,10 +11,18 @@ namespace Admin\Controller;
 //工单控制器
 use Think\Controller;
 
+use Admin\Model\CustomerModel;
+
 class WoController extends Controller{
 
     public function index()
     {
+        if($_SESSION['user']['grade'] == 3 || $_SESSION['user']['grade']==4){
+            $customer = new CustomerModel();
+            $res = $customer->where(array('customer_id'=>$_SESSION['user']['id']))->field('province,city,name,grade')->find();
+            $this->assign('info',$res);//客户信息
+        }
+
         $this->display();
     }
 

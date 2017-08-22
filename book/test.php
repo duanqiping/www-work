054a1c91c873d5aff5bdeba23b431ca1d2fe74b1
qiping <pksanwei@163.com>
2017-08-22 18:42:49 +0800

    荒废了一天

--- book/test.php
+++ book/test.php
@@ -1,27 +1,6 @@
 <?php  
 
-date_default_timezone_set("Asia/Shanghai");
-
-$time = 25*3600+4820;
-
-$time_day = floor($time/(24*3600));//天数
-
-$time_hour =floor( ($time%(3600*24)) /( 3600) );//小时 //1220
-
-
-$time_m =floor( ($time%(3600)) / 60 );//分钟
-
-$time_s =floor( ($time%(3600)) % 60 );//秒
-
-
-
-var_dump($time);
-var_dump($time_day);
-var_dump($time_hour);
-
-var_dump($time_m);
-
-var_dump($time_s);
+echo count('abc');
 
 
 

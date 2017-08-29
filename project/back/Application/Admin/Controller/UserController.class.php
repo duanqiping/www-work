97b68287e65e365f72aa23dbbc13cb175f21ab98
qiping <pksanwei@163.com>
2017-08-29 10:47:46 +0800

    用户管理 中列表和手环绑定分离出来 代码重构

--- project/back/Application/Admin/Controller/UserController.class.php
+++ project/back/Application/Admin/Controller/UserController.class.php
@@ -8,31 +8,24 @@
 
 namespace Admin\Controller;
 
-
-use Admin\Model\DeviceModel;
-use Admin\Model\UserModel;
 use Think\Controller;
 
 //用户管理控制器
-class UserController extends Controller{
+class UserController extends BaseController
+{
     //用户管理首页
     public function index()
     {
-        $uid = $_SESSION['user']['id'];
+        $uid = $this->uid;
         $condition = $_GET?$_GET:array();
 
-        $user = new UserModel();
-
-        $customer = D('customer');
-        $school_res = $customer->where(array('customer_id'=>$uid))->field('name,province,city,type,school_type')->find();
+        $user = D('user');
 
         $res = $user->_list(makeCondition($condition,$uid,$contest_sn=0),$current=$_GET['current']);
 
         $deptInfo = $user->getDept($uid);//获取系别
         $gradeInfo = $user->getGrade($uid,$condition['dept']);//获取年级
         $classInfo = $user->getClass($uid,$condition['dept'],$condition['grade']);//获取班级
-
-        $this->assign('customerInfo',$school_res);
 
         $this->assign('_list', $res);
         $this->assign('condition', $condition);
@@ -47,13 +40,23 @@ class UserController extends Controller{
         $this->display();
     }
 
+    //手环绑定
+    public function bind()
+    {
+        $customer = D('customer');
+        $school_res = $customer->where(array('customer_id'=>$this->uid))->field('name,province,city,type,school_type')->find();
+
+        $this->assign('customerInfo',$school_res);
+
+
+        $this->display();
+    }
+
     //添加用户 编码与用户一一对应关系
     public function add()
     {
-        $uid = $_SESSION['user']['id'];
-
-        $user = new UserModel();
-        $device = new DeviceModel();
+        $user = D('user');
+        $device = D('device');
 
         $data = I('post.');
         $data['sex'] = $data['sex']=='男'?1:2;
@@ -62,13 +65,12 @@ class UserController extends Controller{
 
         $device_id = $device->checkDeviceCode($code);//检查手环编码
         if(!$device_id){
-            var_dump( $device->getError());
-            exit();
+            exit($device->getError());
         }
 
         //检查用户是否存在
         $condition['studentId'] = $data['studentId'];
-        $condition['customer_id'] = $uid;
+        $condition['customer_id'] = $this->uid;
         $user_res = $user->where($condition)->field('user_id')->find();
         if($user_res){
             //存在 更新用户信息和关联手环编码
@@ -85,24 +87,21 @@ class UserController extends Controller{
             //不存在 创建账号和关联手环编码
             $data['add_time'] = NOW_TIME;
             $data['school'] = $_SESSION['user']['name'];
-            $data['customer_id'] = $uid;
+            $data['customer_id'] = $this->uid;
 
             $user_id = $user->add($data);
             $device->where(array('device_id'=>$device_id))->setField('user_id',$user_id);
         }
 
-        $this->redirect('index');
+        $this->redirect('bind');
     }
 
     //获取系别
     public function getDept()
     {
-        $customer_id = $_SESSION['user']['id'];
-
         $schoolInfo = D('schoolInfo');
-        $dept_res = $schoolInfo->where(array('customer_id'=>$customer_id))->field('dept_name')->select();
-//        echo $schoolInfo->_sql();
-//        exit();
+        $dept_res = $schoolInfo->where(array('customer_id'=>$this->uid))->field('dept_name')->select();
+
         echo json_encode($dept_res);
     }
 
@@ -110,10 +109,9 @@ class UserController extends Controller{
     public function getGrade()
     {
         $dept = I('get.dept');
-        $customer_id = $_SESSION['user']['id'];
 
         $schoolInfo = D('schoolInfo');
-        $grade_num = $schoolInfo->where(array('customer_id'=>$customer_id,'dept_name'=>$dept))->getField('grade_num');
+        $grade_num = $schoolInfo->where(array('customer_id'=>$this->uid,'dept_name'=>$dept))->getField('grade_num');
 
 //        file_put_contents('log.txt',$grade_num."0000"."\n",FILE_APPEND );
 
@@ -124,11 +122,10 @@ class UserController extends Controller{
     public function getClass()
     {
         $dept = I('get.dept');
-        $customer_id = $_SESSION['user']['id'];
 
         $schoolInfo = D('schoolInfo');
         $condition = array();
-        $condition['customer_id'] = $customer_id;
+        $condition['customer_id'] = $this->uid;
         if($dept) $condition['dept_name'] = $dept;
 
         $class_list = $schoolInfo->where($condition)->getField('class_list');
